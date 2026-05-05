2f5ab52757ececcbd436d4848b0c19879913a131
Linsong Wang <blinsowa@amazon.com>
2026-05-05 15:53:18 -0700

    update

--- astro-starter/astro.config.mjs
+++ astro-starter/astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
+	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
 	integrations: [react()],
 });
